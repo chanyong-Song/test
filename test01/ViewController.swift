43e69ced35dfdb9f645f5a7699378b6bac42ae6b
song <cksdyd1991@naver.com>
2020-08-12 12:19:44 +0900

    add var

--- test01/ViewController.swift
+++ test01/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var a: Int = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
